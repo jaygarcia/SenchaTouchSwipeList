3bbcf4614f80b726ea8ab25e30ceabe3e6b72c9e
Ballsacian1 <JordanLaramie@gmail.com>
2011-04-27 19:30:37 -0400

    Added default configs

--- Ext.ux.ListSwipe.js
+++ Ext.ux.ListSwipe.js
@@ -5,7 +5,15 @@ Ext.ux.ListSwipe = Ext.extend(Ext.Container, {
     swipeDirection : 'left',
     defaultType    : 'button',
     initComponent : function() {
-
+		var config = {
+			layout: {
+				type: 'hbox',
+				align: 'left',
+				pack: 'start'
+			}
+		};
+		
+		Ext.apply(this, Ext.applyIf(this.initialConfig, config));
         Ext.ux.ListSwipe.superclass.initComponent.call(this);
     },
     init : function(parent) {
